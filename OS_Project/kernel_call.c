33c4dc8c53f404ed313ad4f318f1b584d57dd081
Ashwin Raghav <ashwinraghav@gmail.com>
2012-09-21 17:17:03 -0400

    #Ashwin, system calls minus caching

--- OS_Project/kernel_call.c
+++ OS_Project/kernel_call.c
@@ -1,7 +1,8 @@
 #include <linux/unistd.h>
 #include <sys/syscall.h>
 #include <timer.h>
-#define __NR_getjiffies 320
+#define FILE_NAME "foo.txt" 
+#define LINK_NAME "bar.txt" 
 
 //function declerations
 
@@ -12,12 +13,15 @@ int measure_kernel_call()
         unsigned long long rdtsc_time = start_rdtsc_timer();
         
         for (int i=0; i<100000;i++)
-        { 
+        {
                 jiffies = syscall( SYS_getpid );
         }
-        
-        rdtsc_time = stop_rdtsc_timer(rdtsc_time, "pid_sys_call");
 
+        for (int i=0; i< 100; i++){
+                syscall(SYS_link, FILE_NAME, LINK_NAME);
+                syscall(SYS_unlink, LINK_NAME);
+        }
+        rdtsc_time = stop_rdtsc_timer(rdtsc_time, "pid_sys_call");
         stop_timer(wall_time, "pid_sys_call");
 
         return 0;
