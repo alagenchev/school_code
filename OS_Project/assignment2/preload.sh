a0f8477e10b992129b54455f5c2f6dcb2a6300d0
alagenchev <alagenchev@gmail.com>
2012-11-21 17:40:03 -0500

    new changes

---
+++ OS_Project/assignment2/preload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Enter the program's name along with parameters."
+	exit 1
+fi
+
+export LD_PRELOAD="$PWD/liboverride.so"
+
+$1 "${@:2}"
+result=$?
+
+export LD_PRELOAD=""
+
+exit $result
