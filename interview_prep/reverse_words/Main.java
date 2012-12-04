70df1b7cbb8887b637a6ef048197d8b7aabc017c
alagenchev <alagenchev@gmail.com>
2012-12-04 15:21:03 -0500

    adding string reverse

--- interview_prep/reverse_words/Main.java
+++ interview_prep/reverse_words/Main.java
@@ -1,7 +1,44 @@
+import java.util.Stack;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+        String toReverse = "This is  a string that Ivan wants to reverse";
+        String reversed = "";
+    
+        String temp = Reverse(toReverse);
+
+        String[] split = temp.split(" ");
+
+        for(String str : split)
+        {
+            reversed += Reverse(str);
+            reversed +=" ";
+        }
+
+        System.out.println("Reversed: " + reversed);
+
 	}
+
+    public static String Reverse(String stringToReverse)
+    { 
+        String reversed = "";
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(char c: stringToReverse.toCharArray())
+        {
+            stack.push(c);
+        }
+       
+        while(!stack.empty())
+        {
+            reversed += stack.pop();
+        }
+
+
+        return reversed;
+
+    }
 }
